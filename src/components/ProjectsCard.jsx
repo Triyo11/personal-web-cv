5902a618b4c85f608c3480179e4ea1dd9a8229ee
Triyo Krismantoro <rusdiantoro117@gmail.com>
2024-03-11 17:01:11 +0700

    add hover effect at visi website button in project card

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,13 +1,27 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
-const ProjectsCard = ({ image, title, desc, role, date, techs, view, link }) => {
-
+const ProjectsCard = ({
+  image,
+  title,
+  desc,
+  role,
+  date,
+  techs,
+  view,
+  link,
+}) => {
   return (
     <div className="p-6 md:mx-32 mx-5 bg-gray-300 flex md:flex-row flex-col gap-4 rounded-2xl">
-      <img className="md:w-[50%] w-full md:h-[17rem] h-[10rem] rounded-md" alt='project-sample' src={image} />
+      <img
+        className="md:w-[50%] w-full md:h-[17rem] h-[10rem] rounded-md"
+        alt="project-sample"
+        src={image}
+      />
       <div className="flex flex-col justify-evenly">
-        <h2 className="md:text-2xl text-xl font-semibold text-gray-900">{title}</h2>
+        <h2 className="md:text-2xl text-xl font-semibold text-gray-900">
+          {title}
+        </h2>
         <p className="md:text-lg text-md text-justify leading-tight text-gray-700">
           {desc}
         </p>
@@ -19,15 +33,27 @@ const ProjectsCard = ({ image, title, desc, role, date, techs, view, link }) => 
             ))}
           </ul>
         </div>
-        <p className="text-gray-900 pt-3">Date: <span>{date}</span></p>
-        <p className="text-gray-900 pt-3">Tech stack: <span>{techs.join(", ")}</span></p>
+        <p className="text-gray-900 pt-3">
+          Date: <span>{date}</span>
+        </p>
+        <p className="text-gray-900 pt-3">
+          Tech stack: <span>{techs.join(", ")}</span>
+        </p>
         <p className="text-gray-900 font-semibold pt-3">View: </p>
         <ul className="flex gap-2 font-semibold">
           {view.map((item) => (
-            <li key={item} className="text-orange-500 p-1">{item}</li>
+            <li key={item} className="text-orange-500 p-1">
+              {item}
+            </li>
           ))}
         </ul>
-        <NavLink to={link} target={"_blank"} className="p-2 mt-2 bg-orange-500 text-white max-w-max rounded-lg">Visit Website</NavLink>
+        <NavLink
+          to={link}
+          target={"_blank"}
+          className="p-2 mt-2 bg-orange-500 text-white max-w-max rounded-lg hover:scale-110 hover:shadow-xl transition-all ease-in-out duration-300"
+        >
+          Visit Website
+        </NavLink>
       </div>
     </div>
   );
@@ -35,13 +61,13 @@ const ProjectsCard = ({ image, title, desc, role, date, techs, view, link }) => 
 
 ProjectsCard.propTypes = {
   image: PropTypes.string,
-  title: PropTypes.string,  
+  title: PropTypes.string,
   desc: PropTypes.string,
   role: PropTypes.array,
   date: PropTypes.string,
   techs: PropTypes.array,
   view: PropTypes.array,
-  link: PropTypes.string
-}
+  link: PropTypes.string,
+};
 
 export default ProjectsCard;
